2f9982f1be97d09f4f2d035d00e30c71f0718361
Andres-Neira <andres.neira.pa@gmail.com>
2025-10-12 20:13:14 -0500

    Cambio de tasa
    General los valores que no son variables se dejan en mayusculas

--- MODULO_2/clase1/Banco.py
+++ MODULO_2/clase1/Banco.py
@@ -7,8 +7,27 @@ class Banco:
     
     @classmethod
     def cambiarTasa(cls,nuevaTasa):
-        cls.tasaInteres=nuevaTasa
+        cls.TASA_INTERES=nuevaTasa
 
     @staticmethod
     def conversionDolares(dolares):
         return dolares*0.85
+
+Banco1=Banco("Bacolompia")
+Banco2=Banco("Banco de Bogotá")
+
+print(Banco1.nombre)
+print(Banco1.TASA_INTERES)
+print(Banco2.nombre)
+print(Banco2.TASA_INTERES)
+
+print(Banco1.nombre)
+Banco1.cambiarTasa(0.4)
+print(Banco1.TASA_INTERES)
+
+
+print(Banco2.nombre)
+Banco2.cambiarTasa(0.2)
+print(Banco2.TASA_INTERES)
+
+print(Banco1.conversionDolares(1000))
